698f062aa803a94e5f7fd0a60d375655dae59fad
Mustang-Robotics <133044972+Mustang-Robotics@users.noreply.github.com>
2024-11-18 11:05:00 -0600

    Update TakeIn.java

--- src/main/java/frc/robot/commands/TakeIn.java
+++ src/main/java/frc/robot/commands/TakeIn.java
@@ -1,1 +1,23 @@
+package frc.robot.commands;
 
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class TakeIn extends Command {
+
+  private final Intake m_intake;
+  private final double m_speed;
+  
+  public TakeIn(Intake intake, double speed) {
+    m_intake = intake;
+    m_speed = speed;
+    addRequirements(m_intake);
+  }
+
+  @Override
+    public void initialize() {
+        m_intake.setSpeed(m_speed); 
+    }
+
+  }
