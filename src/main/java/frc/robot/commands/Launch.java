5d3f724e2776ee0979e8081355ae703ef62260c4
Mustang-Robotics <133044972+Mustang-Robotics@users.noreply.github.com>
2024-11-18 11:12:33 -0600

    Create Launch.java

---
+++ src/main/java/frc/robot/commands/Launch.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class Launch extends Command {
+
+  private final Intake m_intake;
+  
+  public TakeIn(Intake intake) {
+    m_intake = intake;
+    addRequirements(m_intake);
+  }
+
+  @Override
+    public void initialize() {
+        m_intake.setSpeed(-1); 
+    }
+
+  }
