4b83d40ff66cd45da02f424be8a26b2e631e6d78
Mustang-Robotics <133044972+Mustang-Robotics@users.noreply.github.com>
2024-11-18 11:34:18 -0600

    Update ToggleArm.java

--- src/main/java/frc/robot/commands/ToggleArm.java
+++ src/main/java/frc/robot/commands/ToggleArm.java
@@ -1,1 +1,19 @@
+package frc.robot.commands;
 
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class ToggleArm extends Command {
+
+  private final Pneumatics m_pneumatics;
+  
+  public ToggleArm(Pneumatics pneumatics) {
+    m_pneumatics = pneumatics;
+    addRequirements(m_pneumatics);
+  }
+
+  @Override
+    public void initialize() {
+        m_pneumatics.toggleArm(); 
+    }
